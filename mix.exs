896cadc2f266608d1a668aea72b9256fb98c72c1
Jun Sumida <jun.sumida@mixi.co.jp>
2016-08-19 15:54:36 +0900

    created VERSION and mix.exs file for hex

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule EredisCluster.Mixfile do
+  use Mix.Project
+
+  @version File.read!("VERSION") |> String.strip
+
+  def project do
+    [app: :erl_creole,
+     version: @version,
+     description: "Converting strings between multibyte encodings and Unicode",
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    []
+  end
+
+  defp deps do
+    []
+  end
+
+  defp package do
+    [files: ~w(src mix.exs README.md COPYING VERSION),
+     maintainers: ["Takeru Ohta"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/sile/erl-creole"}]
+  end
+end
