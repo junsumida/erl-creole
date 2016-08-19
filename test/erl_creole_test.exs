06dba627ffca81fb84d2cf877ba9eec5c0542e30
Jun Sumida <jun.sumida@mixi.co.jp>
2016-08-19 17:11:35 +0900

    add a basic test on from_string and convert

--- test/erl_creole_test.exs
+++ test/erl_creole_test.exs
@@ -1,8 +1,17 @@
 defmodule ErlCreoleTest do
   use ExUnit.Case
-  doctest ErlCreole
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test ".from_string and convert" do
+    eucjp = '日本語のmojiretsuですよ' |>  :creole.from_string(:eucjp)
+    sjis  = '日本語のmojiretsuですよ' |>  :creole.from_string(:sjis)
+
+    assert is_binary(eucjp)
+    assert is_binary(sjis)
+
+    from_eucjp = eucjp |> :creole.convert(:eucjp, :utf8)
+    from_sjis  = sjis  |> :creole.convert(:sjis,  :utf8)
+
+    assert "日本語のmojiretsuですよ" == from_eucjp
+    assert "日本語のmojiretsuですよ" == from_sjis
   end
 end
